2fcc2c77d7c83463aa5afb59655b9721ea2199c9
Callum Leslie <github@cleslie.uk>
2022-04-26 17:25:14 +0100

    Add constructors and functions

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -10,4 +10,17 @@ pub struct Block {
     data: Vec<Transaction>,
 }
 
-impl Block {}
+impl Block {
+    pub fn new(prev_hash: String, data: Vec<Transaction>) -> Block {
+        return Block {};
+    }
+    pub fn genesis() -> Block {
+        return Block {};
+    }
+    pub fn next_block(last_block: Block, data: Vec<Transaction>) -> Block {
+        return Block {};
+    }
+    pub fn block_details(&self) -> &str {
+        return "";
+    }
+}
