7895d6339662797a575c2a953b3774bb527e97fc
Callum Leslie <github@cleslie.uk>
2022-04-26 20:49:49 +0100

    Example / test of the blockchain in action

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,18 @@ use blockchain::Blockchain;
 
 fn main() {
     let mut bc = Blockchain::new();
+    bc.add_pending_transaction(
+        "callum".to_string(),
+        "not callum".to_string(),
+        "3000".to_string(),
+    );
+    bc.add_pending_transaction(
+        "not callum".to_string(),
+        "callum".to_string(),
+        "1000".to_string(),
+    );
+    bc.add_block();
     println!("{}", bc.chain[0].block_details());
+    println!("{}", bc.chain[1].block_details());
+    println!("{:?}", bc.verify());
 }
