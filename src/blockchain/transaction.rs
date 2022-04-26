b662d22231c57522cc264c731fc3dcea9770cc33
Callum Leslie <github@cleslie.uk>
2022-04-26 16:58:31 +0100

    Add transaction class and constructor

--- src/blockchain/transaction.rs
+++ src/blockchain/transaction.rs
@@ -1,3 +1,21 @@
-pub struct Transaction {}
+use chrono::prelude::*;
 
-impl Transaction {}
+pub struct Transaction {
+    pub timestamp: i64,
+    pub source: String,
+    pub destination: String,
+    pub block_id: i64
+    pub data: String,
+}
+
+impl Transaction {
+    pub fn new(source: String, destination: String, block_id: i64) -> Transaction {
+        return Transaction {
+            timestamp: Utc::now().timestamp(),
+            source: source,
+            destination: destination,
+            block_id: block_id,
+            data: data,
+        }
+    }
+}
