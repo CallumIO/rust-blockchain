d54762ab9c25e5da0a86ba834bf9aad7990393d7
Callum Leslie <github@cleslie.uk>
2022-04-26 20:00:55 +0100

    Implement blockchain class and constructor

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,5 +3,16 @@ mod transaction;
 use block::Block;
 use transaction::Transaction;
 
-pub struct Blockchain {}
-impl Blockchain {}
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+    pending_transactions: Vec<Transaction>,
+}
+
+impl Blockchain {
+    pub fn new() -> Blockchain {
+        return Blockchain {
+            chain: vec![Block::genesis()],
+            pending_transactions: vec![],
+        };
+    }
+}
