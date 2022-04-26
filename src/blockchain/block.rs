19fe82eeaff0241000fd368aefa52f4dc93db549
Callum Leslie <github@cleslie.uk>
2022-04-26 17:14:51 +0100

    Add block properties

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -2,6 +2,12 @@ use crate::blockchain::transaction::Transaction;
 use chrono::prelude::*;
 use sha2::{Digest, Sha256};
 
-pub struct Block {}
+pub struct Block {
+    timestamp: i64,
+    hash: String,
+    block_id: i64,
+    prev_hash: String,
+    data: Vec<Transaction>,
+}
 
 impl Block {}
