e13a55eb57ac1abf00fe9b92c28d0f4f2e2f48c2
Callum Leslie <github@cleslie.uk>
2022-04-26 17:31:09 +0100

    Add convertor for byte array to hex string

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -24,3 +24,7 @@ impl Block {
         return "";
     }
 }
+pub fn byte_array_to_hex(bytes: Vec<u8>) -> String {
+    let strs: Vec<String> = bytes.iter().map(|b| format!("{:02X}", b)).collect();
+    return strs.join(" ");
+}
