0fded922048598cf4155aa832a8948880d57929d
Callum Leslie <github@cleslie.uk>
2022-04-26 18:02:47 +0100

    Add block properties and constructor function

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -1,18 +1,36 @@
 use crate::blockchain::transaction::Transaction;
 use chrono::prelude::*;
 use sha2::{Digest, Sha256};
+use std::convert::TryInto;
 
 pub struct Block {
     timestamp: i64,
     hash: String,
     block_id: i64,
     prev_hash: String,
-    data: Vec<Transaction>,
+    data: String,
 }
 
 impl Block {
-    pub fn new(prev_hash: String, data: Vec<Transaction>) -> Block {
-        return Block {};
+    pub fn new(prev_hash: String, block_id: i64, data: String) -> Block {
+        let timestamp = Utc::now().timestamp();
+        let mut sha = Sha256::new();
+        let to_hash: Vec<String> = [timestamp.to_string(), prev_hash, data.to_JSON_string()];
+        sha.update(to_hash.join(""));
+        let hash_bytes = sha.finalize();
+        let hash = byte_array_to_hex(
+            hash_bytes
+                .as_slice()
+                .try_into()
+                .expect("Error hashing block"),
+        );
+        return Block {
+            timestamp: timestamp,
+            hash: hash,
+            block_id: block_id,
+            prev_hash: prev_hash,
+            data: data,
+        };
     }
     pub fn genesis() -> Block {
         return Block {};
